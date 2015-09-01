fe9fd207e6510a2f2aa08018d750765c198cfc14
Simon Prickett <simon@crudworks.org>
2015-09-01 14:38:10 -0700

    Now caches crosswalk API responses (forever).

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ var port = process.env.PORT || 8888;
 var Factual = require('factual-api');
 var factual = new Factual(process.env.FACTUAL_KEY, process.env.FACTUAL_SECRET);
 
+var crosswalkCache = {};
+
 router.route('/places').get(
 	function(request, response) {
 		// TODO check parameters
@@ -38,17 +40,27 @@ router.route('/places').get(
 
 router.route('/crosswalk').get(
 	function(request, response) {
-		factual.get('/t/crosswalk?filters={"factual_id": "' + request.query.id + '"' + (request.query.namespace ? ', "namespace": "' + request.query.namespace + '"' : '') + '}',
-			function (error, res) {
-				if (! error) {
-					response.jsonp(res.data);
-				} else {
-					// TODO error case...
-					console.log(error);
-					response.jsonp([]);
+		// Check if we have this in the cache as we only get 500 crosswalk queries a day
+		var cacheKey = 'cacheKey' + request.query.id + (request.query.namespace || '');
+		var cachedData = crosswalkCache.cacheKey;
+
+		if (cachedData) {
+			console.log('Serving ' + cacheKey + ' from crosswalk API cache.');
+			response.jsonp(cachedData);
+		} else {
+			factual.get('/t/crosswalk?filters={"factual_id": "' + request.query.id + '"' + (request.query.namespace ? ', "namespace": "' + request.query.namespace + '"' : '') + '}',
+				function (error, res) {
+					if (! error) {
+						crosswalkCache.cacheKey = res.data;
+						response.jsonp(res.data);
+					} else {
+						// TODO error case...
+						console.log(error);
+						response.jsonp([]);
+					}
 				}
-			}
-		);
+			);
+		}
 	}
 );
 
