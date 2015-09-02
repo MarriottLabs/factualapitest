31bc552fa37f77acc358f8c6f47400a4d83a2853
Simon Prickett <simon@crudworks.org>
2015-09-02 07:54:05 -0700

    Fixed localhost API URL issue.

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,6 @@
 var app = {
-	//API_BASE_URL: 'https://factualserver.marriottlabs.com',
-	API_BASE_URL: 'http://localhost:8888',
+	API_BASE_URL: 'https://factualserver.marriottlabs.com',
+	//API_BASE_URL: 'http://localhost:8888',
 	GOOGLE_MAPS_KEY: 'AIzaSyB6gr2x_5tRO07och-OMr44GFjGA_zmGxc',
 	ALPHABET: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
 
